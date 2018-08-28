b2a00cc5514d360934421dd865c3e752571098d7
Nicolas Bertin <nicolas.bertin0@gmail.com>
2018-08-28 17:31:09 +0200

    add items method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,22 @@
 class CartsController < ApplicationController
   def show
-    @cart = Cart.find_by user_id: current_user.id
+    @cart = current_cart
     @total_price = total_price
   end
 
+  def add_item
+    @cart = current_cart
+    @cart << Item.find(params[:id])
+    flash.now[:notice] = "Item added to your cart"
+    redirect_to items_path
+  end
+
   def total_price
-    @cart = Cart.find_by user_id: current_user.id
+    @cart = current_cart
     @cart.items.to_a.sum { |item| item.price }
+  end
+
+  def current_cart
+    Cart.find_by user_id: current_user.id
   end
 end
